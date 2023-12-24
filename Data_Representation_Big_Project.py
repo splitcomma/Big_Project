12593a76c0d9cf7d922bbd8b662fcdd74a1c67f7
splitcomma <mozijegy@hotmail.com>
2023-12-24 14:47:18 +0000

    adding config api key

--- Data_Representation_Big_Project.py
+++ Data_Representation_Big_Project.py
@@ -1,3 +1,16 @@
 # Data Representation - Big project
+from flask import Flask
 
-#test test
+import pandas as pd
+import requests
+import json
+#from config import cfg
+
+
+
+#apikey = cfg("HQrevkey")
+url = "https://market-watch-api.datascience.hqrevenue.com/openapi.json"
+
+response = requests.get(url)
+data = response.json()
+print(data)
